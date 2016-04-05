2a7665444ee72545de25e3ae575a49bb6d93df3b
hall2cm <hall2cm@gmail.com>
2016-04-05 18:07:59 -0400

    Update 1.0.2

--- polls/views.py
+++ polls/views.py
@@ -20,12 +20,18 @@ class IndexView(generic.ListView):
         published in the future).
         """
         return Question.objects.filter(
-            pub_date__lte_timezone.now()
+            pub_date__lte=timezone.now()
         ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
